9a5a1fafa430de3523f80e55119d8e9e44101aab
antoxdt <182255429+antoxdt@users.noreply.github.com>
2025-09-11 23:33:42 -0600

    Fundamentos de JS primera sesión

---
+++ js/03-variables.js
@@ -0,0 +1,40 @@
+/* 
+
+vaiables 
+
+
+son contenedores que almacenan datos. En JS, pueden declarar variables utilizando las palabras clave "var", "let" y "const".
+
+var: es la forma mas antigua de declarar variables. Tiene un alcance global o de función y puede ser redeclarada y actualizada.
+
+let: introducida en ES6, tiene un alcance de bloque y puede ser actualizada pero redeclararse dentro del mismo enlace.
+
+const:tambien introducida en ES6, tiene un enlace de bloque y no puede ser ni redeclarada ni actualizada. Se utiliza para valores que no deben cambiar.
+
+*/
+
+
+
+/* declaración de var */
+
+var nombre = "Ant";
+console.log(nombre); //Ant
+
+nombre = "Juan"; // Actualización
+
+
+/* Declaracion con let */
+
+let edad = 20;
+console.log(edad);
+
+edad = 15 // Actualización
+
+
+
+/* Declaracion con const */
+
+const PI = 3.1416;
+console.log(PI);
+
+/* PI = 3.1875; */ // Esto da error, no se puede actualizar una constante
